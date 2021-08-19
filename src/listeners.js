5c616c99057e7b68f9c8cc24332bb0f754fac5f7
Anvi Alex <epononanvialex@gmail.com>
2021-08-19 19:20:29 +0300

    Add Background-Switcher feature

--- src/listeners.js
+++ src/listeners.js
@@ -1,9 +1,11 @@
 import { enjoy } from "./control.js";
 import { list } from "./song.js";
+import { changeBackground } from "./changeBackground.js";
 
 const play = document.querySelector('#play');
 const playNext = document.querySelector('#next');
 const playPrevious = document.querySelector('#back');
+const switcherBackground = document.querySelector('.switcher');
 
 
 play.addEventListener('click', () =>{
@@ -17,6 +19,6 @@ play.addEventListener('click', () =>{
   }
 });
 
-
+switcherBackground.addEventListener('click', changeBackground);
 playNext.addEventListener('click', enjoy.playNext);
 playPrevious.addEventListener('click', enjoy.playPrevious);
