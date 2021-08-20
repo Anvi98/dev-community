e8930ef541096971301398fd368c8e7ab19d42a1
Anvi Alex <epononanvialex@gmail.com>
2021-08-20 12:40:56 +0300

    Add input chat

--- src/toogleDiscussion.js
+++ src/toogleDiscussion.js
@@ -1,9 +1,11 @@
 export const toogleDiscussion = () => {
   const containerChat = document.querySelector('.g-discussion');
+  const arrow = document.querySelector('.arrow');
+
   let list = [...containerChat.classList];
 
   if(list.includes('hide')) {
-    containerChat.classList.remove('hide')
+    containerChat.classList.remove('hide');
   } else {
     containerChat.classList.add('hide');
   }
