bce49578413d18e9f9b8c0f9bc9ac54d2eecd15f
Anvi Alex <epononanvialex@gmail.com>
2021-08-22 22:25:59 +0300

    small update

--- app.js
+++ app.js
@@ -1,16 +1,3 @@
 import './src/listeners.js';
 import './src/control.js';
 
-
-// let max = enjoy.list.length;
-
-
-
-// export let num_song = 0;
-// export let song = new Audio(listSong[num_song]);
-// let player = true;
-
-// if(player){
-
-// }
-
